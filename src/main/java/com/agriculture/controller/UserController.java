8227ed581dd2e75c3149c617dab450b426fc1aa3
LiuYZhuL <1296937385@qq.com>
2025-05-25 15:18:27 +0800

    *Mapper接口类都创建完毕 目前select返回值为对应po、list<po>、int、boolean update和delete无返回值

---
+++ src/main/java/com/agriculture/controller/UserController.java
@@ -0,0 +1,54 @@
+package com.agriculture.controller;
+
+import com.agriculture.model.dto.LoginUser;
+import com.agriculture.model.po.User;
+import com.agriculture.service.impl.UserServiceImpl;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+import java.util.Objects;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+    @Autowired
+    private UserServiceImpl userService;
+    @GetMapping("/login")
+    public ModelAndView login() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("redirect:/api/user/login");
+        return modelAndView;
+    }
+    @PostMapping("/login")
+    public ModelAndView login(@Valid LoginUser loginUser,
+                              BindingResult bindingResult,
+                              HttpSession session){
+        ModelAndView modelAndView = new ModelAndView();
+        if (bindingResult.hasErrors()) {
+            modelAndView.setViewName("redirect:/api/user/login");
+            String errorMsg = Objects.requireNonNull(bindingResult.getFieldError()).getDefaultMessage();
+            modelAndView.addObject("error", errorMsg);
+            modelAndView.addObject("loginUser", loginUser);
+            return modelAndView;
+        }
+        User user;
+        try {
+            user = userService.login(loginUser);
+        } catch (RuntimeException runtimeException) {
+            modelAndView.setViewName("redirect:/api/user/login");
+            modelAndView.addObject("error", runtimeException.getMessage());
+            modelAndView.addObject("loginUser", loginUser);
+            return modelAndView;
+        }
+        session.setAttribute("user", user);
+        modelAndView.setViewName("redirect:/api/dashboard");
+        return modelAndView;
+    }
+}
