722336533e5ee6904884c995df3da66b892086bf
LiuYZhuL <1296937385@qq.com>
2025-05-25 15:25:32 +0800

    登录的GET与POST

--- src/main/java/com/agriculture/controller/UserController.java
+++ src/main/java/com/agriculture/controller/UserController.java
@@ -5,17 +5,17 @@ import com.agriculture.model.po.User;
 import com.agriculture.service.impl.UserServiceImpl;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
 import java.util.Objects;
 
-@RestController
+@Controller
 @RequestMapping("/api/user")
 public class UserController {
     @Autowired
