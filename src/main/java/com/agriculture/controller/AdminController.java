880dd154cdb2c60343a1a44d913ad763301728f3
LiuYZhuL <1296937385@qq.com>
2025-05-26 20:52:14 +0800

    管理员分页查询用户

--- src/main/java/com/agriculture/controller/AdminController.java
+++ src/main/java/com/agriculture/controller/AdminController.java
@@ -21,6 +21,8 @@ public class AdminController {
     public ModelAndView manage(){
         ModelAndView mv = new ModelAndView();
         mv.setViewName("manage");
+        PageInfo<User> pageInfo = userService.listUsers(1, 5, new User());
+        mv.addObject("pageInfo", pageInfo);
         return mv;
     }
     //修改用户状态
@@ -28,37 +30,41 @@ public class AdminController {
     public ModelAndView updateUserStatus(
             @RequestParam("userId") Integer userId,
             @RequestParam("status") Integer status){
-      ModelAndView mv = new ModelAndView();
-      try{
-         userService.updateUserStatus(userId,status);
-          mv.addObject("success", true);
-          mv.addObject("message", "用户状态更新成功");
-          mv.setViewName("manage");
-          return mv;
-      }catch (RuntimeException e) {
-          mv.addObject("success", false);
-          mv.addObject("message", e.getMessage());
-          mv.setViewName("manage");
-          return mv;
-      }
- }
-    //获取用户列表
-@GetMapping("/manage/user")
-public ModelAndView getAllUsers(){
-    ModelAndView mv = new ModelAndView("manage");
-    try {
-        List<User> userList = userService.AllUsers();
-        mv.addObject("userList", userList);
-        mv.addObject("success", true);
-        mv.addObject("message", "获取所有用户成功");
-    } catch (RuntimeException e) {
-        mv.addObject("success", false);
-        mv.addObject("message", e.getMessage());
+        ModelAndView mv = new ModelAndView();
+        try{
+            userService.updateUserStatus(userId,status);
+            mv.addObject("success", true);
+            mv.addObject("message", "用户状态更新成功");
+            mv.setViewName("manage");
+            return mv;
+        }catch (RuntimeException e) {
+            mv.addObject("success", false);
+            mv.addObject("message", e.getMessage());
+            mv.setViewName("manage");
+            return mv;
+        }
     }
-    return mv;
-}
-    //获取用户信息
+    //获取用户列表
+    @GetMapping("/manage/user")
+    public ModelAndView listUsers(
+            @RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum,
+            @RequestParam(value = "pageSize", defaultValue = "5") Integer pageSize,
+            User user){
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("activeSection","userMgt");
+        try{
+            PageInfo<User> pageInfo = userService.listUsers(pageNum, pageSize, user);
+            mv.addObject("pageInfo", pageInfo);
+            mv.addObject("success", true);
+            mv.addObject("message", "用户列表获取成功");
+            mv.setViewName("manage");
+            return mv;
+        }catch (RuntimeException e) {
+            mv.addObject("success", false);
+            mv.addObject("message", e.getMessage());
+            mv.setViewName("manage");
+            return mv;
+        }
+    }
 
-
-    //修改用户角色
 }
